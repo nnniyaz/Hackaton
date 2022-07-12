35055c01f3242ab664a40c2b4810d5e5371e3df6
Niyaz Nassyrov <nassyrovich@gmail.com>
2022-07-12 17:57:42 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Images
+import nightLife from './images/party.jpeg'
+import cafe from './images/cafe.jpeg'
+import bday from './images/bday.webp'
+import resort from './images/pool.jpeg'
+import library from './images/library.jpeg'
+import britain from './images/uk.webp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='title'>
+        Almaty Guideliance
+      </div>
+
+      <div className='table'>
+        <div className='row'>
+          <div className='cell' style={{ backgroundImage: `url(${nightLife})` }}>
+            <div className='subsell'>Night Life</div>
+          </div>
+          <div className='cell' style={{ backgroundImage: `url(${cafe})` }}>
+            <div className='subsell'>Where to eat</div>
+          </div>
+          <div className='cell' style={{ backgroundImage: `url(${bday})` }}>
+            <div className='subsell' style={{ fontSize: '28px' }}>Places for BDay</div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='cell' style={{ backgroundImage: `url(${resort})` }}>
+            <div className='subsell'>Resorts</div>
+          </div>
+          <div className='cell' style={{ backgroundImage: `url(${library})` }}>
+            <div className='subsell' style={{ textAlign: 'center' }}>Libraries Coworkings</div>
+          </div>
+          <div className='cell' style={{ backgroundImage: `url(${britain})` }}>
+            <div className='subsell'>English Clubs</div>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
